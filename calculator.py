feb6110f1a59ffefc13834c0f8745bf6b258a4f8
TedoreGhlonti <tedore.ghlonti@gmail.com>
2026-02-20 17:54:07 +0000

    hello

--- calculator.py
+++ calculator.py
@@ -1,5 +1,8 @@
-x = float(input("What's x? "))
-y = float(input("What's y? "))
-z = round(x + y, 2)
-print(z)
+def main():
+    x = int(input('What is x? '))
+    print('x is ', square(x))
 
+def square(n):
+    return n * n
+
+main()
---
+++ hello.py
@@ -0,0 +1,7 @@
+def hello(to):
+    print('Hello,', to)
+
+name = input("What's your name? ").strip().title()
+
+hello(name)
+
