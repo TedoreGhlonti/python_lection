673341591ed97c8f7c4c6cad6336dd1998ccd16b
TedoreGhlonti <tedore.ghlonti@gmail.com>
2026-02-23 17:32:22 +0000

    grade

--- conditional/compare.py
+++ conditional/compare.py
@@ -1,9 +1,7 @@
 x = int(input("What's x? "))
 y = int(input("What's y? "))
 
-if x < y:
-    print("x is less then y")
-elif x > y:
-    print("x is greater then y")
+if x < y or x > y:
+    print("x is not equal to y")
 else:
-    print("x is equal y")
+    print("x is equal to y")
