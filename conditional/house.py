662f7e1f0c9a0f53f88a2b23b3196274936bb9d7
TedoreGhlonti <tedore.ghlonti@gmail.com>
2026-02-27 17:31:48 +0000

    house

---
+++ conditional/house.py
@@ -0,0 +1,7 @@
+name = input('whats your name?: ')
+if name == "Harry" or name == "Hermion" or name == "Ron":
+ print("Griffindor")
+elif name == "Draco":
+  print("Slygherin")
+else:
+  print("Who?")
