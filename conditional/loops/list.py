d3171872563741b2c6292c38dd8d8e3a56df0bad
TedoreGhlonti <tedore.ghlonti@gmail.com>
2026-03-13 17:18:23 +0000

    list

---
+++ conditional/loops/list.py
@@ -0,0 +1,12 @@
+results = ["Mario", "Luigi"]
+results.append('Princess')
+results.append('Yoshi')
+results.append('Kuupa Troopa')
+results.append('Toad')
+results.append(["Bowser", "Donkey Konng JR"])
+results.remove(["Bowser", "Donkey Konng JR"])
+results.extend(["Bowser", "Donkey Konng JR"])
+results.remove("Bowser")
+results.insert(0, "Bowser")
+print(results)
+
---
+++ conditional/loops/phone.py
@@ -0,0 +1,5 @@
+def main():
+    phone = '595110232'
+    print(phone[0:9:1])
+    
+main()
